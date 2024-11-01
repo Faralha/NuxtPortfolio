b18a21f6ddb1110e94cd69ee3be53fbd4485a596
Zufar <69440085+Faralha@users.noreply.github.com>
2024-11-02 02:48:16 +0700

    feat: aresta, basic landing page, other web porto stuff

---
+++ pages/aresta.vue
@@ -0,0 +1,640 @@
+<template>
+	<div class="wrapper">
+		<div class="wrapper-inner">
+			<div class="background" id="bg">
+				<div class="container" id="headline">
+					<p class="aresta">aresta 17</p>
+					<h1 class="tema">
+						Modernisasi:<br />membawa perubahan,<br />memajukan peradaban
+					</h1>
+					<p class="tanggal">27-29 januari 2022</p>
+					<a href="/register" class="tombol" target="_blank">daftar</a>
+				</div>
+			</div>
+			<div class="trailer" id="trailer">
+				<iframe
+					class="frame"
+					src="https://www.youtube.com/embed/7l4Nih6lKzM"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen
+					>></iframe
+				>
+			</div>
+			<div class="container" id="lomba">
+				<h1 class="perlombaan">perlombaan</h1>
+				<p class="keterangan">15 Lomba, Puluhan Juta.</p>
+				<div class="list-lomba">
+					<ul class="kolom">
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1lLG8jcafgXSMl_shjxrIGnQXcvyDq_uv/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/arab.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Olimpiade Bahasa Arab"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1P9hIFHGyhleLwAC6pZKs2PKhflXNRsV0/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/business-plan.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Business Plan"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1RNWzpAFO_uM1awTs3yNjnjz3y7jFWm3c/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/dai-cilik.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Dai Cilik"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1khhVzCGWxD21Xe0Xhzpc_pACtqyh6QXn/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/desain-poster.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Desain Poster"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1gryFgel0dSWVndjR5yhOqXZleaIRnn8S/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/dramatisasi-puisi.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Dramatisasi Puisi"
+							/></a>
+						</li>
+					</ul>
+					<ul class="kedua">
+						<li>
+							<a
+								href="https://drive.google.com/file/d/11vqKufsTSSTTzxVjkt_F5gZB0uopq2nI/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/fotografi.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Fotografi"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1mjmAS1o72NmWiYCgn7O352SyhmhBJq7t/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/khitobah.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Khitobah"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1dje_yzcpi8glNBWkVAZeF1UrHNmn1geR/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/LDBI.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba LDBI"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1yKwpgZ0WF78ZCaXy8Oh0yC-akIGnJ8TE/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/mhq-sd.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba MHQ SD"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1StXiiTI9ZlC2E0rkl5C589ZoJ_gbWX8i/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/mhq-smp.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba MHQ SMP"
+							/></a>
+						</li>
+					</ul>
+					<ul class="ketiga">
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1ZuJOAgtfuIxGms29-7THexL-HkjddevT/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/mhq-sma.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba MHQ SMA"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1NQIP9oWjOHLVmF89J4dKDt1X2RTIQlFK/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/nasyid.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Nasyid"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1DCm39GJ_nLAVme0g31gH2UvL0qYw53zW/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/story-telling.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Story Telling"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1c7TKaf-CzYxR2dfxMbmxIlNgNm0SUCOq/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/survival-camp.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Survival Camp"
+							/></a>
+						</li>
+						<li>
+							<a
+								href="https://drive.google.com/file/d/1H6a3TtX1awYZJCgvcCXecEaB4WeWduz5/view?usp=sharing"
+								target="_blank"
+								rel="noopener"
+								><img
+									src="/img/lomba/video-kreatif.png"
+									width="auto"
+									height="200"
+									alt="Icon Lomba Video Kreatif"
+							/></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<footer>
+				<section class="ft-main">
+					<div class="ft-main-item" id="logo">
+						<img
+							src="../resources/LOGO_TRACE_BLACK.svg"
+							alt="Logo OSHK"
+							width="100"
+							height="auto" />
+					</div>
+
+					<div class="ft-main-item">
+						<h2 class="ft-title">Tentang Kami</h2>
+						<ul>
+							<li><a href="#" target="_blank" rel="noopener">ARESTA?</a></li>
+							<li>
+								<a
+									href="https://husnulkhotimah.sch.id"
+									target="_blank"
+									rel="noopener"
+									>PONPES HUSNUL KHOTIMAH</a
+								>
+							</li>
+							<li><a href="#" target="_blank" rel="noopener">OSHK</a></li>
+							<li><a href="#" target="_blank" rel="noopener">CODERESTA</a></li>
+						</ul>
+					</div>
+
+					<div class="ft-main-item">
+						<h2 class="ft-title">Kontak</h2>
+						<ul>
+							<li>
+								<a
+									href="https://goo.gl/maps/fkRbehSFMPoXnjLf7"
+									target="_blank"
+									rel="noopener"
+									>KANTOR PUSAT</a
+								>
+							</li>
+							<li>
+								<a
+									href="https://wa.me/6285156962968?text=Saya%20ingin%20bertanya%20perihal%20Aresta"
+									target="_blank"
+									rel="noopener"
+									>0851 5696 2968 (Humas)</a
+								>
+							</li>
+							<li>
+								<a
+									href="https://wa.me/6285863704512?text=Saya%20ingin%20bertanya%20perihal%20Aresta"
+									target="_blank"
+									rel="noopener"
+									>0858 6370 4512 (Customer Support)</a
+								>
+							</li>
+							<li>
+								<a
+									href="mailto:aresta.husnulkhotimah@gmail.com"
+									target="_blank"
+									rel="noopener"
+									>aresta.husnulkhotimah@gmail.com</a
+								>
+							</li>
+						</ul>
+					</div>
+				</section>
+
+				<section class="ft-social">
+					<ul class="ft-social-list">
+						<li>
+							<a
+								href="https://www.instagram.com/aresta.official/"
+								target="_blank"
+								rel="noopener"
+								><i class="fab fa-instagram"></i
+							></a>
+						</li>
+						<li>
+							<a
+								href="https://www.youtube.com/c/arestatv"
+								target="_blank"
+								rel="noopener"
+								><i class="fab fa-youtube"></i
+							></a>
+						</li>
+						<li>
+							<a
+								href="https://www.tiktok.com/@official.aresta?"
+								target="_blank"
+								rel="noopener"
+								><i class="fab fa-tiktok"></i
+							></a>
+						</li>
+						<li>
+							<a
+								href="mailto:aresta.husnulkhotimah@gmail.com"
+								target="_blank"
+								rel="noopener"
+								><i class="fas fa-envelope"></i
+							></a>
+						</li>
+						<li>
+							<a
+								href="https://wa.me/6285863704512?text=Saya%20ingin%20bertanya%20perihal%20Aresta"
+								target="_blank"
+								rel="noopener"
+								><i class="fab fa-whatsapp"></i
+							></a>
+						</li>
+					</ul>
+				</section>
+
+				<section class="ft-weem">
+					<h1 class="weem">Made with love by coderesta</h1>
+				</section>
+			</footer>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+.wrapper {
+	background-color: #000000 !important;
+	margin: 0px;
+  padding-top: 50px;
+}
+
+
+.prompt {
+	background-color: #ff00c3;
+}
+
+.prompt .container p {
+	color: black;
+	margin: 0px;
+	font-family: "Montserrat";
+	font-size: 1rem;
+	text-align: center;
+	font-weight: 800;
+	padding-top: 0.5rem;
+	padding-bottom: 0.5rem;
+	text-transform: uppercase;
+	padding-right: 2rem;
+	padding-left: 2rem;
+}
+
+p.aresta {
+	font-family: "Montserrat";
+	text-transform: uppercase;
+	font-weight: 900;
+	font-size: 2rem;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: #ff00c3;
+	color: black;
+}
+
+h1.tema {
+	font-family: "Montserrat";
+	font-weight: 900;
+	text-transform: uppercase;
+	font-size: 3rem;
+	color: #ff00c3;
+}
+
+p.tanggal {
+	font-family: "Montserrat";
+	font-weight: 700;
+	text-transform: capitalize;
+	font-size: 1.5rem;
+	color: #ff00c3;
+}
+
+a.tombol {
+	font-family: "Montserrat";
+	font-weight: 500;
+	text-transform: uppercase;
+	color: #ff00c3;
+	padding: 15px 20px;
+	outline: 2px solid;
+	text-decoration: none;
+}
+
+a.tombol:hover {
+	color: black;
+	background-color: #ff00c3;
+	transition: 0.4s ease all;
+}
+
+h1.perlombaan {
+	text-transform: uppercase;
+	font-family: "Montserrat";
+	font-weight: 900;
+	font-size: 5rem;
+	color: #ff00c3;
+	text-align: center;
+}
+
+p.keterangan {
+	font-family: "Montserrat";
+	font-size: 2rem;
+	font-weight: 900;
+	text-transform: uppercase;
+	text-align: center;
+	-webkit-text-stroke-width: 1px;
+	-webkit-text-stroke-color: #ff00c3;
+	color: black;
+}
+
+p.keterangan,
+h1.perlombaan {
+	padding: 0;
+	margin: 0;
+}
+
+.trailer#trailer {
+	margin: auto;
+	background-image: url(/img/trailerv2.svg);
+	background-repeat: no-repeat;
+	background-size: contain;
+}
+
+.trailer {
+	padding-top: 0.3vh;
+	position: relative;
+	overflow: hidden;
+}
+
+iframe.frame {
+	margin: auto;
+	height: 34vw;
+	width: 60vw;
+	display: block;
+}
+
+.list-lomba {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+
+.list-lomba ul {
+	padding-left: 2rem;
+	padding-right: 2rem;
+}
+
+.list-lomba ul li {
+	display: flex;
+	padding: 6rem 2rem 6rem 2rem;
+}
+
+.list-lomba ul li a:hover {
+	filter: sepia();
+	transition: 0.4s ease all;
+}
+
+a {
+	color: inherit;
+	text-decoration: none;
+}
+
+#gambar {
+	width: 100vh;
+}
+
+#headline {
+	height: 100vh;
+	padding-top: 25vh;
+}
+
+#bg {
+	background-image: url("/img/bg-v2.png");
+	background-repeat: no-repeat;
+	background-size: cover;
+	height: 100vh;
+}
+
+#trailer {
+	height: 100vh;
+	margin: 0;
+}
+
+#lomba {
+	height: auto;
+}
+
+/* Footer Section */
+
+footer {
+	background-color: #ff00c3;
+}
+
+.ft-title {
+	color: black;
+	font-size: 1.2rem;
+	padding-bottom: 0.625rem;
+	font-family: "Montserrat";
+	font-weight: 800;
+	text-transform: uppercase;
+}
+
+.ft-main {
+	padding: 1.25rem 1.875rem;
+	padding-bottom: 0;
+	display: flex;
+	flex-wrap: wrap;
+}
+
+.ft-main-item {
+	padding: 1.25rem;
+	min-width: 12.5rem;
+}
+
+.ft-main-item#logo {
+	min-width: 1rem !important;
+}
+
+.ft-main-item ul li a:hover {
+	color: white;
+	font-weight: 700;
+}
+
+ul {
+	padding: 0;
+}
+
+li {
+	list-style: none;
+}
+
+ul li a {
+	color: black;
+	font-family: "Montserrat";
+	font-weight: 400;
+	font-size: 0.95rem;
+}
+
+.ft-social {
+	padding: 0 1.875rem;
+}
+
+.ft-social-list {
+	display: flex;
+	justify-content: center;
+}
+
+.ft-social-list li {
+	margin: 0.5rem;
+	font-size: 1.25rem;
+	list-style: none;
+}
+
+.ft-social-list li a {
+	font-size: 1.2rem;
+}
+
+.ft-social-list li a:hover {
+	color: #47c0b4;
+}
+
+.ft-weem {
+	background-color: #47c0b4;
+}
+
+.weem {
+	font-family: "Montserrat";
+	font-weight: 900;
+	color: black;
+	text-align: center;
+	font-size: 0.8rem;
+	padding: 0.5rem;
+	text-transform: uppercase;
+}
+
+/* Media Queries */
+@media screen and (max-width: 900px) {
+	.ft-main {
+		flex-direction: column;
+	}
+	.list-lomba ul {
+		display: flex;
+		flex-direction: column;
+		margin-left: 100vw;
+		margin-right: 100vw;
+	}
+	h1.tema {
+		font-size: 10vw;
+	}
+	p.aresta {
+		font-size: 7vw;
+		margin: 0;
+	}
+	p.tanggal {
+		font-size: 5vw;
+	}
+	a.tombol {
+		font-size: 4vw;
+		padding: 2vw 4vw;
+	}
+	h1.perlombaan {
+		font-size: 10vw;
+	}
+	p.keterangan {
+		font-size: 5vw;
+	}
+	iframe.frame {
+		width: 79vw;
+		height: 42vw;
+	}
+	.trailer#trailer {
+		background-image: url("/img/trailerv2_mobile.png");
+		background-repeat: no-repeat;
+		background-size: contain;
+	}
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="wrapper">
+		<div
+			class="container d-flex flex-column justify-content-center align-items-center"
+			style="height: 100vh">
+			<h1>Faralha's Portfolio.</h1>
+			<p>It is currently under development for an uncertain amount of time.</p>
+
+			<div class="d-flex gap-4">
+				<NuxtLink to="/aresta">Aresta 17</NuxtLink>
+				<a href="https://tropicasweets.com" target="_blank">Tropica Sweets</a>
+				<a href="https://academy.alterkaiscans.my.id" target="_blank"
+					>Alterkai TL Academy</a
+				>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+  .wrapper {
+    background-color: black;
+    opacity: 90%;
+    color: white;
+  }
+
+  p {
+    opacity: 60%;
+  }
+
+  a {
+    color: inherit;
+    opacity: 85%;
+    font-weight: bold;
+
+    padding: 5px 10px 5px 10px;
+    border: 0.5px solid white;
+    border-radius: 5px;
+    transition: all 0.2s ease-in;
+  }
+
+  a:hover {
+    background-color: white;
+    color: black;
+  }
+</style>
